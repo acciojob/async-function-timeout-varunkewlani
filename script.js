6281f9cb9d47a03fa58f32ccf468e68e38e91290
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-08-26 18:47:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+let text=document.getElementByID("text").value;
+let delay=document.getElementByID("delay").value;
+let btn=document.getElementByID("btn");
+let output=document.getElementByID("output");
+
+const promise=()=>
+	{
+		return new Promise((resolve,reject)=>
+			{
+				setTimeout(()=>
+				{
+					resolve(text);
+				},delay)
+			})
+	}
+}
+btn.addEventListener("click",async function 
+{
+	try 
+	{
+		let result = await delayText(text, delay);
+        output.innerHTML = result;	
+	} 
+	catch (error) 
+	{
+		console.log(error)
+	}
+});
